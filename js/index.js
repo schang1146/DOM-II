9772ec21d638fdd0b93429c3dceca1b87bbdce81
schang1146 <sammychang2185@gmail.com>
2019-04-13 20:55:16 -0700

    finished event listeners #3 & #4

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 
 // Event Listener #1
 const imgIntro = document.querySelector('.home .intro img');
-imgIntro.addEventListener('mouseover', function() {
+imgIntro.addEventListener('mouseover', function(event) {
     TweenMax.to(imgIntro, 1, {x:100});
 });
-imgIntro.addEventListener('mouseout', function() {
+imgIntro.addEventListener('mouseout', function(event) {
     TweenMax.to(imgIntro, 1, {x:0});
     console.log(imgIntro);
 });
@@ -13,18 +13,33 @@ imgIntro.addEventListener('mouseout', function() {
 // Event Listener #2
 const navLink = document.querySelectorAll('.nav-link');
 navLink.forEach(link => {
-    link.addEventListener('mousedown', function() {
+    link.addEventListener('mousedown', function(event) {
         link.style['color'] = 'blue';
         link.style['font-weight'] = 'bold';
     });
-    link.addEventListener('mouseup', function() {
+    link.addEventListener('mouseup', function(event) {
         link.removeAttribute('style');
     })
 });
 
 // Event Listener #3
+const textContent = document.querySelectorAll('.text-content, .destination');
+textContent.forEach(content => {
+    content.addEventListener('dblclick', function(event) {
+        content.style['border'] = '1px dashed blue';
+    })
+})
 
 // Event Listener #4
+const bodyTextColor = document.querySelector('body');
+window.addEventListener('scroll', function(event) {
+    let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    if (bodyTextColor.style['color'] != 'violet') {
+        bodyTextColor.style['color'] = colors[colors.indexOf(bodyTextColor.style['color'])+1];
+    } else {
+        bodyTextColor.style['color'] = 'red';
+    }
+})
 
 // Event Listener #5
 
